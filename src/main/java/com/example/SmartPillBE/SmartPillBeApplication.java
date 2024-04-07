2d51f06312b011db4f8e9317fd0527eceb829653
JIYE KIM <hyo37009@gmail.com>
2024-04-07 22:28:53 +0900

    project initiallization

---
+++ src/main/java/com/example/SmartPillBE/SmartPillBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.SmartPillBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPillBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartPillBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SmartPillBE/SmartPillBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SmartPillBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPillBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
