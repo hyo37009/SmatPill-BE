6e2d073c9a423c14b5ac4f76467d86aa6de7750e
a210113 <jangeunseo567@gmail.com>
2024-04-26 23:33:41 +0900

    test

--- src/main/java/com/example/SmartPillBE/domain/Pill.java
+++ src/main/java/com/example/SmartPillBE/domain/Pill.java
@@ -4,9 +4,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 public class Pill {
 
     @Id
