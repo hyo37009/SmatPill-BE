e09966b968609dcb2ea3e1ae8db49d67e0ccd854
JIYE KIM <hyo37009@gmail.com>
2024-05-13 22:59:16 +0900

    .

--- src/main/java/com/example/SmartPillBE/ocr/OCRGeneralAPI.java
+++ src/main/java/com/example/SmartPillBE/ocr/OCRGeneralAPI.java
@@ -3,25 +3,19 @@ package com.example.SmartPillBE.ocr;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-public class OCRGeneralAPIDemo {
+public class OCRGeneralAPI {
+    String apiURL = "https://7jsiorbx1j.apigw.ntruss.com/custom/v1/30311/04ab83d4215870a0d025063f80730961413c0f2e4844704d1e88853624aa4397/general";
+    String secretKey = "dEd5ZkZScmd2a01XZVB1d3FTR0dKa250cGhUT3dQRE8=";
 
-    public static void main(String[] args) {
-        String apiURL = "https://7jsiorbx1j.apigw.ntruss.com/custom/v1/30311/04ab83d4215870a0d025063f80730961413c0f2e4844704d1e88853624aa4397/general";
-        String secretKey = "dEd5ZkZScmd2a01XZVB1d3FTR0dKa250cGhUT3dQRE8=";
-
-        OCRGeneralAPIDemo ocr = new OCRGeneralAPIDemo();
-        String result = ocr.getOCRResult(apiURL, secretKey);
-        System.out.println("result = " + result);
-
-    }
-
-    public String getOCRResult(String apiURL, String secretKey) {
-
+    public List<String> getOCRResult(String imagePath) {
+        List<String> result = new ArrayList<>();
         try {
             URL url = new URL(apiURL);
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
@@ -39,7 +33,7 @@ public class OCRGeneralAPIDemo {
             JSONObject image = new JSONObject();
             image.put("format", "jpg");
 //            image should be public, otherwise, should use data
-            File file = new File("C:\\Users\\user\\Documents\\github\\SmatPill-BE\\src\\main\\java\\com\\example\\SmartPillBE\\ocr\\asd.jpg");
+            File file = new File(imagePath);
             FileInputStream inputStream = new FileInputStream(file);
             byte[] buffer = new byte[inputStream.available()];
             inputStream.read(buffer);
@@ -67,15 +61,16 @@ public class OCRGeneralAPIDemo {
             StringBuffer response = new StringBuffer();
             while ((inputLine = br.readLine()) != null) {
                 response.append(inputLine);
+                result.add(inputLine);
             }
             br.close();
 
             System.out.println(response);
-            return response.toString();
+            return result;
         } catch (Exception e) {
             System.out.println(e);
         }
-        return "False";
+        return null;
 
 
     }
